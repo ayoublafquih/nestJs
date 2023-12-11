829fff6741eab8afbb2e89f43b4aee8bd730ebeb
Ayoub_LAFQUIH <ayoub.lafquih@outlook.com>
2023-12-11 13:13:20 +0000

    add Prisma and create user bookmark module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+@Injectable({})
+export class AuthService {
+
+    constructor(private prismaService: PrismaService) { }
+
+    signup() {
+
+        return { msg: 'i am signup' };
+    }
+
+    signin() {
+        return 'i am signin';
+    }
+
+}
